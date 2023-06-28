3468ebe8a6c696326c0105f94e74ca83b276ed9b
Dyadyushko Marat <Dwarf-2008@bk.ru>
2023-06-28 20:42:59 +0300

    ktrl2

--- Program.cs
+++ Program.cs
@@ -4,3 +4,14 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+void PrintArrayString(string [] array)
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i != array.Length - 1) Console.Write($"\"{array[i]}\", ");
+        else Console.Write($"\"{array[i]}\"");
+    }
+    Console.WriteLine("]");
+}
